efaf141c66f77af6ea93794826cc020f41e8a389
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-08-29 03:00:21 -0400

    Start character build

---
+++ SlayTheSpire2.NewKunlun/SlayTheSpire2.NewKunlunCode/Powers/InternalDamagePower.cs
@@ -0,0 +1,92 @@
+﻿using BaseLib.Hooks;
+using Godot;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace SlayTheSpire2.NewKunlun.SlayTheSpire2.NewKunlunCode.Powers;
+
+public class InternalDamagePower : NewKunlunPower
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public static async Task Apply(
+        PlayerChoiceContext choiceContext,
+        Creature target,
+        Decimal amount,
+        Creature? applier,
+        CardModel? cardSource,
+        bool silent = false
+    )
+    {
+        // TODO: Hook modifiers.
+        await PowerCmd.Apply<InternalDamagePower>(
+            choiceContext,
+            target,
+            amount,
+            applier,
+            cardSource,
+            silent
+        );
+    }
+
+    public override async Task AfterSideTurnStart(
+        CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState
+    )
+    {
+        if (!participants.Contains(Owner))
+            return;
+
+        await PowerCmd.Decrement(this);
+        Flash();
+    }
+
+    public override async Task AfterDamageReceived(
+        PlayerChoiceContext choiceContext,
+        Creature target,
+        DamageResult result,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource
+    )
+    {
+        if (target != Owner || result.UnblockedDamage <= 0)
+            return;
+
+        var damage = Amount;
+        await PowerCmd.Remove(this);
+        Flash();
+        await CreatureCmd.Damage(
+            choiceContext,
+            Owner,
+            damage,
+            ValueProp.Unblockable | ValueProp.Unpowered,
+            Owner
+        );
+    }
+
+    public override IEnumerable<HealthBarForecastSegment> GetHealthBarForecastSegments(
+        HealthBarForecastContext context
+    )
+    {
+        return
+        [
+            new HealthBarForecastSegment()
+            {
+                Amount = Amount,
+                Color = new Color(0.6f, 0.1f, 0.1f),
+                AffectsHpLabel = true,
+                Direction = HealthBarForecastDirection.FromRight,
+                LeftOriginLayout = HealthBarForecastLeftOriginLayout.Chained,
+            },
+        ];
+    }
+}
---
+++ SlayTheSpire2.NewKunlun/SlayTheSpire2.NewKunlunCode/Variables/InternalDamageVar.cs
@@ -0,0 +1,26 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace SlayTheSpire2.NewKunlun.SlayTheSpire2.NewKunlunCode.Variables;
+
+public class InternalDamageVar(string name, Decimal damage) : DynamicVar(name, damage)
+{
+    public const string DefaultName = "InternalDamage";
+
+    public InternalDamageVar(Decimal damage)
+        : this(DefaultName, damage) { }
+
+    public override void UpdateCardPreview(
+        CardModel card,
+        CardPreviewMode previewMode,
+        Creature? target,
+        bool runGlobalHooks
+    )
+    {
+        // TODO: Hook modifiers.
+        var value = BaseValue;
+        PreviewValue = value;
+    }
+}
