16ef8c6a4f46ed57aa0fb4c7ee5656c0faabd782
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-09-07 01:34:21 -0400

    Bug fix

--- NewKunlun/NewKunlunCode/Powers/TalismanDetonatePower.cs
+++ NewKunlun/NewKunlunCode/Powers/TalismanDetonatePower.cs
@@ -48,7 +48,6 @@ public partial class TalismanDetonatePower : NewKunlunPower
         if (IsUpgraded)
             CardCmd.Upgrade(card, CardPreviewStyle.None);
         await CardPileCmd.AddGeneratedCardToCombat(card, PileType.Hand, player);
-        card.ExhaustAtEndOfTurn = true;
         await PowerCmd.Remove(this);
         Flash();
     }
